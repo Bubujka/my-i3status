317e5955a6e34f1f610ac77e234dd5da5572f64c
Amado Martinez <amado@projectivemotion.com>
2016-09-01 05:07:48 -0500

    Allow passing arguments to i3status.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 var child_process = require('child_process');
 var path = require('path');
 var fn = function(d) {return d; };
+var i3statusargs    =   process.argv.splice(2);
 
-if(process.argv.length > 2){
+if(i3statusargs.length > 0){
     fn  =   require(
-            path.resolve(process.cwd(), process.argv[2])
+            path.resolve(process.cwd(), i3statusargs.pop())
         );
 }else if(path.join(process.env.HOME, '.myi3status/')){
   fn = require(path.join(process.env.HOME, '.myi3status/'));
@@ -14,7 +15,7 @@ if(process.argv.length > 2){
 }
 
 child_process
-  .spawn('i3status').stdout.on('data', function(data){
+  .spawn('i3status', i3statusargs).stdout.on('data', function(data){
     var d = data.toString();
     if(d.match(/^,\[.*\]/)){
       console.log(','+JSON.stringify(fn(JSON.parse(d.replace(/^,/,'')))));
